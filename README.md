907a3cc999ae82d4ff2ea18d4d281fca76bee664
Jay <jlincoln88@gmail.com>
2020-02-03 00:47:46 -0500

    init nextjs

---
+++ README.md
@@ -0,0 +1,3 @@
+# Project page for my person site
+
+Coming soon!
---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Index() {
+  return (
+    <div>
+      <p>Webpage for John coming soon!</p>
+    </div>
+  );
+}
+
